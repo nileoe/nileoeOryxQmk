2c369eec7eaf44158705758d4e83202c54fd81c0
nileoe <linoperdrix@gmail.com>
2025-03-17 14:18:38 +0000

    corrected one handed chords

--- 3WWgD/keymap.c
+++ 3WWgD/keymap.c
@@ -17,9 +17,9 @@ enum custom_keycodes {
 const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
     LAYOUT(
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
-        'L', '*', '*', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
-        'L', '*', '*', '*', '*', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
+        '*', '*', '*', '*', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
                             'L', '*',  '*', 'R'
     );
 
