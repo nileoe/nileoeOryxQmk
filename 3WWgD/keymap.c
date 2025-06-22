760c78da45805848fdc6387073ab770875d814ff
nileoe <linoperdrix@gmail.com>
2025-06-22 16:53:01 +0200

    removed osl accent layer

--- 3WWgD/keymap.c
+++ 3WWgD/keymap.c
@@ -79,20 +79,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     KC_MS_ACCEL2,   KC_MS_ACCEL1,                                   TO(0),          KC_MS_BTN2
   ),
   [5] = LAYOUT_voyager(
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          ST_MACRO_3,     ST_MACRO_4,     ST_MACRO_5,     KC_NO,          KC_NO,          
-    KC_NO,          KC_NO,          KC_GRAVE,       LSFT(KC_6),     KC_QUOTE,       LSFT(KC_QUOTE),                                 KC_NO,          KC_RIGHT_SHIFT, KC_NO,          KC_NO,          KC_NO,          KC_NO,          
-    KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
-                                                    KC_NO,          KC_NO,                                          KC_NO,          KC_NO
-  ),
-  [6] = LAYOUT_voyager(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           TO(0),          
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_ENTER,       
     KC_LEFT_CTRL,   KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_UP,          KC_ENTER,       
     KC_LEFT_SHIFT,  KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_LEFT,        KC_DOWN,        KC_RIGHT,       
                                                     KC_SPACE,       KC_LEFT_ALT,                                    KC_ESCAPE,      KC_SPACE
   ),
-  [7] = LAYOUT_voyager(
+  [6] = LAYOUT_voyager(
     KC_Y,           KC_4,           KC_3,           KC_2,           KC_1,           KC_0,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           TO(0),          
     KC_U,           KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
     KC_I,           KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOTE,       
